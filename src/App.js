9ddbd960167de34a3913328a98dadb8c8ca67737
Ryan <37783403+RyanKeepRunning@users.noreply.github.com>
2019-10-06 23:01:09 +1100

    dynamic frame

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-
+import Frame from './containers/Frame/Frame';
 import Counter from './containers/Counter/Counter';
 import './App.css';
 
 class App extends Component {
+  state={
+    products:[],
+    isLoading:true,
+  }
+  componentDidMount(){
+      setTimeout(()=>{
+        console.log('props change')
+          this.setState({products:['Placeholder1','Placeholder2'],isLoading:false})
+      },3000)
+  }
   render() {
     return (
       <div className="App">
+        <Frame products={this.state.products} height={123} isLoading={this.state.isLoading}/>
+        <br/>
        <Counter />
       </div>
     );
